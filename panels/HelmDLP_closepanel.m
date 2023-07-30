1192346d43462074ee5423d15ac6ba861cc7ba2f
Hai Zhu <zhuh1106@gmail.com>
2023-07-29 23:39:12 -0400

    add unit test for helmholtz kernel split quadr

--- panels/HelmDLP_closepanel.m
+++ panels/HelmDLP_closepanel.m
@@ -2,8 +2,10 @@ function A = HelmDLP_closepanel(omega,t,s,a,b,side, meth)
 % special quadrature for 2d helmholtz
 % via kernel split Laplace DLP special quadrature
 
+if nargin == 0, test_HelmDLP_closepanel(); return; end
+
 p = numel(s.x); % GL nodes order
-if isfield('be',s), be = s.be; else, be = 1; end  % upsample factor of GL nodes
+if isfield(s,'be'), be = s.be; else, be = 1; end  % upsample factor of GL nodes
 if be > 1
   [x, w] = gauss(p); w = w';
   [xf, wf] = gauss(ceil(be*p)); wf = wf';
@@ -25,4 +27,55 @@ A = ( u + 2/pi*log(abs(s.x(:).'-t.x(:))).*imag(u) + 1/(2*pi)*real(s.nx(:).'./(s.
          - 2/pi*(lapslpmat./s.w').*imag(u) - 1/(2*pi)*real(lapdlpmat); 
 if be > 1, A = A*Imn; end
 
+end
+
+function test_HelmDLP_closepanel
+% test a bvp
+
+k = 10;                       % wave number           
+a = .3; w = 5;                % smooth wobbly radial shape params
+verb = 1;                     % 0 text only; 1 makes a figure
+s = wobblycurve(1,a,w,100);   % parametrix descrip of curve (dummy # pts)
+Np = 16*6;                      % # panels (not a convergence study, just a test)
+p = 16;                       % panel order
+[pa tpan s] = quadr_uniform_panels(s,Np,p);
+zpan = s.Z(tpan);             % panel endpoint locations, in C plane
+
+%%% self mat
+Smat = 1i/4*besselh(0,k*abs(s.x(:).'-s.x(:))).*s.w(:)'; % naive eval mat
+Dmat = 1i/4*(k*abs(s.x(:).'-s.x(:))).*besselh(1,k*abs(s.x(:).'-s.x(:))).*imag(s.wxp(:).'./(s.x(:)-s.x(:).'));
+for i=1:Np % close eval
+  pa{i}.be = 2; % upsample factor
+  cflag=(min(abs(s.x((i-1)*p+1:i*p).'-s.x(:)),[],2)/sum(s.w((i-1)*p+1:i*p))<=0.7); % close targ rule
+  Smat(cflag,(i-1)*p+1:i*p) = HelmSLP_closepanel(k,struct('x',s.x(cflag)),pa{i},zpan(i),zpan(i+1),'e','h');
+  Dmat(cflag,(i-1)*p+1:i*p) = HelmDLP_closepanel(k,struct('x',s.x(cflag)),pa{i},zpan(i),zpan(i+1),'e','h');
+end
+
+%%% solve
+z0=0.3+0.1i; % interior source
+rhs=besselh(0,k*abs(z0-s.x));
+rho = (Smat+Dmat)\rhs; % Helmholtz SLP
+
+%%% eval mat
+x0 = 1.2; nx = 100; gx = max(abs(s.x))*linspace(-x0,x0,nx); % grid for soln errors...
+[xx yy] = meshgrid(gx); zz = xx+1i*yy; ii = ~s.inside(zz);  % outside
+g.x = zz(ii(:));
+Stmat = 1i/4*besselh(0,k*abs(s.x(:).'-g.x(:))).*s.w(:)'; % naive eval mat
+Dtmat = 1i/4*(k*abs(s.x(:).'-g.x(:))).*besselh(1,k*abs(s.x(:).'-g.x(:))).*imag(s.wxp(:).'./(g.x(:)-s.x(:).'));
+for i=1:Np % close eval
+  pa{i}.be = 2; % upsample factor
+  cflag=(min(abs(s.x((i-1)*p+1:i*p).'-g.x(:)),[],2)/sum(s.w((i-1)*p+1:i*p))<=0.7); % close targ rule
+  Stmat(cflag,(i-1)*p+1:i*p) = HelmSLP_closepanel(k,struct('x',g.x(cflag)),pa{i},zpan(i),zpan(i+1),'e','h');
+  Dtmat(cflag,(i-1)*p+1:i*p) = HelmDLP_closepanel(k,struct('x',g.x(cflag)),pa{i},zpan(i),zpan(i+1),'e','h');
+end
+Unum = (Stmat+Dtmat)*rho;
+Uexa = besselh(0,k*abs(z0-g.x));
+
+%%% error
+err = nan*(1+1i)*zz;
+err(ii(:)) = abs(Unum - Uexa);
+
+figure(1),clf,imagesc(log10(abs(err)));colorbar
+axis equal
+fprintf('S+D max err = %.3g\n',max(err(:)))
 end
--- panels/HelmSLP_closepanel.m
+++ panels/HelmSLP_closepanel.m
@@ -2,8 +2,10 @@ function A = HelmSLP_closepanel(omega,t,s,a,b,side, meth)
 % special quadrature for 2d helmholtz
 % via kernel split Laplace SLP special quadrature,
 
+if nargin == 0, test_HelmSLP_closepanel(); return; end
+
 p = numel(s.x); % GL nodes order
-if isfield('be',s), be = s.be; else, be = 1; end  % upsample factor of GL nodes
+if isfield(s,'be'), be = s.be; else, be = 1; end  % upsample factor of GL nodes
 if be > 1
   [x, w] = gauss(p); w = w';
   [xf, wf] = gauss(ceil(be*p)); wf = wf';
@@ -24,4 +26,51 @@ A = ( u + 2/pi*log(abs(s.x(:).'-t.x(:))).*imag(u) ) ... % S_0k(r,r') = S_k(r,r')
          - 2/pi*(lapslpmat./s.w').*imag(u); % -2/pi*log|r-r'|*Im{S_k(r,r')}
 if be > 1, A = A*Imn; end
 
+end
+
+function test_HelmSLP_closepanel
+% test a bvp
+
+k = 10;                       % wave number           
+a = .3; w = 5;                % smooth wobbly radial shape params
+verb = 1;                     % 0 text only; 1 makes a figure
+s = wobblycurve(1,a,w,100);   % parametrix descrip of curve (dummy # pts)
+Np = 16*6;                      % # panels (not a convergence study, just a test)
+p = 16;                       % panel order
+[pa tpan s] = quadr_uniform_panels(s,Np,p);
+zpan = s.Z(tpan);             % panel endpoint locations, in C plane
+
+%%% self mat
+Smat = 1i/4*besselh(0,k*abs(s.x(:).'-s.x(:))).*s.w(:)'; % naive eval mat
+for i=1:Np % close eval
+  pa{i}.be = 2; % upsample factor
+  cflag=(min(abs(s.x((i-1)*p+1:i*p).'-s.x(:)),[],2)/sum(s.w((i-1)*p+1:i*p))<=0.7); % close targ rule
+  Smat(cflag,(i-1)*p+1:i*p) = HelmSLP_closepanel(k,struct('x',s.x(cflag)),pa{i},zpan(i),zpan(i+1),'e','h');
+end
+
+%%% solve
+z0=0.3+0.1i; % interior source
+rhs=besselh(0,k*abs(z0-s.x));
+rho = Smat\rhs; % Helmholtz SLP
+
+%%% eval mat
+x0 = 1.2; nx = 100; gx = max(abs(s.x))*linspace(-x0,x0,nx); % grid for soln errors...
+[xx yy] = meshgrid(gx); zz = xx+1i*yy; ii = ~s.inside(zz);  % outside
+g.x = zz(ii(:));
+Stmat = 1i/4*besselh(0,k*abs(s.x(:).'-g.x(:))).*s.w(:)'; % naive eval mat
+for i=1:Np % close eval
+  pa{i}.be = 2; % upsample factor
+  cflag=(min(abs(s.x((i-1)*p+1:i*p).'-g.x(:)),[],2)/sum(s.w((i-1)*p+1:i*p))<=0.7); % close targ rule
+  Stmat(cflag,(i-1)*p+1:i*p) = HelmSLP_closepanel(k,struct('x',g.x(cflag)),pa{i},zpan(i),zpan(i+1),'e','h');
+end
+Unum = Stmat*rho;
+Uexa = besselh(0,k*abs(z0-g.x));
+
+%%% error
+err = nan*(1+1i)*zz;
+err(ii(:)) = abs(Unum - Uexa);
+
+figure(1),clf,imagesc(log10(abs(err)));colorbar
+axis equal
+fprintf('S max err = %.3g\n',max(err(:)))
 end
